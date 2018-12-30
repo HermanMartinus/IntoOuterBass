ff838f0249a1c968f5badee02da42c57a1289754
Herman Martinus <hfbmartinus@gmail.com>
2018-12-30 16:26:25 +0200

    Waves!

---
+++ Assets/Scripts/AudioSourceGetSpectrum.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioSourceGetSpectrum: MonoBehaviour
+{
+    LineDrawer lineDrawer;
+
+    private void Start()
+    {
+        lineDrawer = new LineDrawer();
+    }
+    void Update()
+    {
+        float[] spectrum = new float[256];
+
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+        for (int i = 1; i < spectrum.Length - 1; i++)
+        {
+            //LineDrawer lineDrawer = new LineDrawer();
+            lineDrawer.DrawLineInGameView(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+
+            //Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
+            //Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
+            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
+            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+        }
+    }
+}
+
+public struct LineDrawer
+{
+    private LineRenderer lineRenderer;
+    private float lineSize;
+
+    public LineDrawer(float lineSize = 0.2f)
+    {
+        GameObject lineObj = new GameObject("LineObj");
+        lineRenderer = lineObj.AddComponent<LineRenderer>();
+        //Particles/Additive
+        lineRenderer.material = new Material(Shader.Find("Hidden/Internal-Colored"));
+
+        this.lineSize = lineSize;
+    }
+
+    private void init(float lineSize = 0.2f)
+    {
+        if (lineRenderer == null)
+        {
+            GameObject lineObj = new GameObject("LineObj");
+            lineRenderer = lineObj.AddComponent<LineRenderer>();
+            //Particles/Additive
+            lineRenderer.material = new Material(Shader.Find("Hidden/Internal-Colored"));
+
+            this.lineSize = lineSize;
+        }
+    }
+
+    //Draws lines through the provided vertices
+    public void DrawLineInGameView(Vector3 start, Vector3 end, Color color)
+    {
+        if (lineRenderer == null)
+        {
+            init(0.2f);
+        }
+
+        //Set color
+        lineRenderer.startColor = color;
+        lineRenderer.endColor = color;
+
+        //Set width
+        lineRenderer.startWidth = lineSize;
+        lineRenderer.endWidth = lineSize;
+
+        //Set line count which is 2
+        lineRenderer.positionCount = 2;
+
+        //Set the postion of both two lines
+        lineRenderer.SetPosition(0, start);
+        lineRenderer.SetPosition(1, end);
+    }
+
+    public void Destroy()
+    {
+        if (lineRenderer != null)
+        {
+            UnityEngine.Object.Destroy(lineRenderer.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WaveGenerator.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+/// Creates a wave form based on an audiosource
+/// Modified version of http://forum.unity3d.com/threads/making-sense-of-spectrum-data.90262
+/// Make sure to plug in the _topLine and _bottomLine line renderers
+public class WaveGenerator : MonoBehaviour
+{
+    AudioSource _audioSource;
+    const int SpectrumSize = 8192;
+    readonly float[] _spectrum = new float[SpectrumSize];
+    public LineRenderer _topLine;
+    public LineRenderer _bottomLine;
+
+    public void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Update()
+    {
+        _audioSource.GetSpectrumData(_spectrum, 0, FFTWindow.BlackmanHarris);
+
+        var bandSize = 1.1f;
+        var crossover = bandSize;
+        var viewSpectrum = new List<float>();
+        var b = 0f;
+        for (var i = 0; i < SpectrumSize; i++)
+        {
+            var d = _spectrum[i];
+            b = Mathf.Max(d, b); // find the max as the peak value in that frequency band.
+            if (i > crossover - 3)
+            {
+                crossover *= bandSize; // frequency crossover point for each band.
+                viewSpectrum.Add(b);
+                b = 0;
+            }
+        }
+
+        SetLinePoints(viewSpectrum, _topLine);
+        SetLinePoints(viewSpectrum, _bottomLine, -1);
+    }
+
+    private void SetLinePoints(List<float> viewSpectrum, LineRenderer lineRenderer, float modifier = 1)
+    {
+        var pointDistance = 0.1f;
+        var width = pointDistance * viewSpectrum.Count;
+
+        lineRenderer.SetVertexCount(viewSpectrum.Count);
+        lineRenderer.SetPositions(viewSpectrum.Select((x, i) => new Vector3(-width / 2 + i * pointDistance, x * 34 * modifier)).ToArray());
+    }
+}
